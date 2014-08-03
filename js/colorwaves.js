69356d2eff6ca01eee5739d809d0d5945d087ffa
Jan Horava <czechinthemail@gmail.com>
2014-08-03 11:14:17 -0700

    improved clarity of pause_state, renamed counter to loop_counter

--- js/colorwaves.js
+++ js/colorwaves.js
@@ -1,29 +1,29 @@
 function recreate_waves(){
     // generate wave HTML
-    var counter = parseInt(document.getElementById('wave-count').value) - 1;
+    var loop_counter = parseInt(document.getElementById('wave-count').value) - 1;
     var wave_html = '';
     do{
-        wave_html += '<div id=' + counter + '></div>';
-    }while(counter--);
+        wave_html += '<div id=' + loop_counter + '></div>';
+    }while(loop_counter--);
     document.getElementById('waves').innerHTML = wave_html;
 
     // set orientation CSS
-    counter = parseInt(document.getElementById('wave-count').value) - 1;
+    loop_counter = parseInt(document.getElementById('wave-count').value) - 1;
     if(document.getElementById('orientation').value == 1){
         // vertical
         do{
-            document.getElementById(counter).style.display = 'inline-block';
-            document.getElementById(counter).style.height = '420px';
-            document.getElementById(counter).style.width = '42px';
-        }while(counter--);
+            document.getElementById(loop_counter).style.display = 'inline-block';
+            document.getElementById(loop_counter).style.height = '420px';
+            document.getElementById(loop_counter).style.width = '42px';
+        }while(loop_counter--);
 
     }else{
         // horizontal
         do{
-            document.getElementById(counter).style.display = 'block';
-            document.getElementById(counter).style.height = '42px';
-            document.getElementById(counter).style.width = '100%';
-        }while(counter--);
+            document.getElementById(loop_counter).style.display = 'block';
+            document.getElementById(loop_counter).style.height = '42px';
+            document.getElementById(loop_counter).style.width = '100%';
+        }while(loop_counter--);
     }
 }
 
@@ -31,7 +31,7 @@ function pause(new_pause_state){
     clearInterval(timer);
 
     pause_state = new_pause_state;
-    if(pause_state){ // if unpaused
+    if(!pause_state){ // if unpaused
         timer = setInterval(
           'update_waves()',
           parseInt(document.getElementById('wave-move-interval').value)
@@ -68,30 +68,30 @@ function reset(){
 function update_waves(){
     // move RGB color generators
     // change direction on collision with edge
-    var counter = 2;
+    var loop_counter = 2;
     do{
-        color_generators[counter * 2] += color_generators[counter * 2 + 1];
-        if(color_generators[counter * 2] > parseInt(document.getElementById('wave-count').value) - 1){
-            color_generators[counter * 2 + 1] = -1;
+        color_generators[loop_counter * 2] += color_generators[loop_counter * 2 + 1];
+        if(color_generators[loop_counter * 2] > parseInt(document.getElementById('wave-count').value) - 1){
+            color_generators[loop_counter * 2 + 1] = -1;
 
-        }else if(color_generators[counter * 2] < 1){
-            color_generators[counter * 2 + 1] = 1;
+        }else if(color_generators[loop_counter * 2] < 1){
+            color_generators[loop_counter * 2 + 1] = 1;
         }
-    }while(counter--);
+    }while(loop_counter--);
 
     // update colors
-    counter = parseInt(document.getElementById('wave-count').value) - 1;
+    loop_counter = parseInt(document.getElementById('wave-count').value) - 1;
     var new_colors = [];
     do{
         // color is based on distance from generator
         new_colors = [
-          9 - Math.abs(counter - color_generators[0]),
-          9 - Math.abs(counter - color_generators[2]),
-          9 - Math.abs(counter - color_generators[4])
+          9 - Math.abs(loop_counter - color_generators[0]),
+          9 - Math.abs(loop_counter - color_generators[2]),
+          9 - Math.abs(loop_counter - color_generators[4])
         ];
 
         // set color CSS, prevent negative values
-        document.getElementById(counter).style.backgroundColor = 
+        document.getElementById(loop_counter).style.backgroundColor = 
           '#'
           + (new_colors[0] > 0
             ? new_colors[0]
@@ -105,21 +105,21 @@ function update_waves(){
             ? new_colors[2]
             : 0
           );
-    }while(counter--);
+    }while(loop_counter--);
 }
 
 var color_generators = [0, 0, 0, 0, 0, 0];
-var pause_state = 0;
+var pause_state = false;
 var timer = 0;
 
 recreate_waves();
 randomize();
-pause(1); // sets project into unpaused state
+pause(pause_state); // sets project into unpaused state
 
 document.getElementById('orientation').onchange = recreate_waves;
 document.getElementById('wave-count').oninput = recreate_waves;
 document.getElementById('wave-move-interval').oninput = function(){
-    pause(pause_state);
+    pause(true);
 };
 
 window.onkeydown = function(e){
